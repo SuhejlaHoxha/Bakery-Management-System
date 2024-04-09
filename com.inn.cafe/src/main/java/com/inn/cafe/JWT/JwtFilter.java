c0982ec4a955c213cc1fd559cf827ff998a4fca5
Fortesa Cena <121317246+Fortesacena@users.noreply.github.com>
2024-04-09 15:40:13 +0200

    11 implementation of migrations (#14)
    * implement-json-token (#8)
    
    * add-login-api-of-user (#9)
    
    * 10 Flyway Implementation (#13)
    
    * add-flyway-implementation
    
    * add-flyway-implementation
    
    * fix-flyway-implementation-and-implement-migrations

---
+++ com.inn.cafe/src/main/java/com/inn/cafe/JWT/JwtFilter.java
@@ -0,0 +1,40 @@
+package com.inn.cafe.JWT;
+
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private CustomerUsersDetailsService service;
+
+    Claims claims = null;
+    private String userName = null;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        if(httpServletRequest.getServletPath().matches("/user/login|/user/forgoPassword|/user/signup")){
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
+        }else{
+
+        }
+    }
+
+
+}
