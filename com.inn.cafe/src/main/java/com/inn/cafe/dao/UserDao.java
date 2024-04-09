1d9e207b48faa42dd311508beb7ad6b646115d86
Fortesa Cena <fortesacena@gmail.com>
2024-04-09 15:36:14 +0200

    fix-flyway-implementation-and-implement-migrations

--- com.inn.cafe/src/main/java/com/inn/cafe/dao/UserDao.java
+++ com.inn.cafe/src/main/java/com/inn/cafe/dao/UserDao.java
@@ -1,6 +1,6 @@
 package com.inn.cafe.dao;
 
-import com.inn.cafe.POJO.User;
+import com.inn.cafe.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
