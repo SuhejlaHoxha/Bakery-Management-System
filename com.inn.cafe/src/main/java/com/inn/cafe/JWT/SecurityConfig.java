018b820e9d8c284ef2fba3f5d9391c55f6a2d80d
Fortesa Cena <fortesacena@gmail.com>
2024-04-09 20:38:07 +0200

    fix-uthenticationConfiguration-problem

--- com.inn.cafe/src/main/java/com/inn/cafe/JWT/SecurityConfig.java
+++ com.inn.cafe/src/main/java/com/inn/cafe/JWT/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -28,6 +29,17 @@ public class SecurityConfig {
 
     @Autowired
     private JwtFilter jwtFilter;
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
