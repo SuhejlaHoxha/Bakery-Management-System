2e1a2e56d468ff6ccdb4dd8ae738fa8981614b36
LauraTafaj <121570865+LauraTafaj@users.noreply.github.com>
2024-04-11 21:30:14 +0200

    Revert "."
    This reverts commit 480173a8f71f465717540ffcda46874d918b6fe0.

--- com.inn.cafe/src/main/java/com/inn/cafe/rest/UserRest.java
+++ com.inn.cafe/src/main/java/com/inn/cafe/rest/UserRest.java
@@ -1,13 +1,10 @@
 package com.inn.cafe.rest;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import wrapper.UserWrapper;
 
-import java.util.List;
 import java.util.Map;
 
 @RequestMapping(path = "/user")
@@ -18,8 +15,5 @@ public interface UserRest {
 
     @PostMapping(path = "/login")
     public ResponseEntity<String> login(@RequestBody(required = true) Map<String,String> requestMap);
-
-    @GetMapping(path = "/get")
-    public ResponseEntity<List<UserWrapper>> getAllUser();
 
 }
--- com.inn.cafe/src/main/java/com/inn/cafe/service/UserService.java
+++ com.inn.cafe/src/main/java/com/inn/cafe/service/UserService.java
@@ -1,17 +1,13 @@
 package com.inn.cafe.service;
 
 import org.springframework.http.ResponseEntity;
-import wrapper.UserWrapper;
 
-import java.util.List;
 import java.util.Map;
 
 public interface UserService {
     ResponseEntity<String> signUp(Map<String, String> requestMap);
 
     ResponseEntity<String> login(Map<String, String> requestMap);
-
-    ResponseEntity<List<UserWrapper>> getAllUser();
 
 
 }
