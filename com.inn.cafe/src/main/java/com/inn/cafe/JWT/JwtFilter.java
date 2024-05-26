0aed9f9cfa1b7126f5cf7f41de7e4f9400b6d21b
Fortesa Cena <121317246+Fortesacena@users.noreply.github.com>
2024-05-27 00:49:54 +0200

    77 swagger UI (#82)
    * swagger-doc
    
    * add-swagger
    
    ---------
    
    Co-authored-by: Ferdeze Nurkasa <ferdeze.nurkasa@student.uni-pr.edu>

--- com.inn.cafe/src/main/java/com/inn/cafe/JWT/JwtFilter.java
+++ com.inn.cafe/src/main/java/com/inn/cafe/JWT/JwtFilter.java
@@ -29,7 +29,7 @@ public class JwtFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
-        if (httpServletRequest.getServletPath().matches("/user/login|/user/forgoPassword|/user/signup")) {
+        if (httpServletRequest.getServletPath().matches("/user/login|/user/forgoPassword|/user/signup|/api/v1/auth/|/v2/api-docs|/v3/api-docs|/v3/api-docs/|/swagger-resources|/swagger-resources/|/configuration/ui|/configuration/security|/swagger-ui/|/webjars/|/swagger-ui.html"))  {
             filterChain.doFilter(httpServletRequest, httpServletResponse);
         } else {
             String authorizationHeader = httpServletRequest.getHeader("Authorization");
